9329461255b2fd371c792e69db8f3815b3b526b4
moamenmamdouh <moamen.mamdouh1@gmail.com>
2021-01-18 22:57:28 +0200

    Chapter3 Finding Web Elements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,7 +1,11 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver; //initiating a driver variable
@@ -9,14 +13,24 @@ public class BaseTests {
     public void SetUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe"); //set chromedriver executable file
         driver = new ChromeDriver(); //assigning a new chromedriver object to the driver variable
+
         driver.get("https://the-internet.herokuapp.com/"); //navigate to a specific url
         driver.manage().window().maximize(); //maximize window size
-        System.out.println(driver.getTitle()); //printing page's title
+
+        WebElement inputLink = driver.findElement(By.linkText("Shifting Content")); //Find element using link text.
+        inputLink.click(); //Click on a web element
+
+        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        example1.click();
+
+        List<WebElement> list = driver.findElements(By.xpath("//ul/li"));
+        System.out.println(list.size());
+
         driver.quit(); //close driver
     }
     //main function
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         BaseTests basetest = new BaseTests();
         basetest.SetUp();
     }
